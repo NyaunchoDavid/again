e4a5c9885118f1c8db6d0356313dc6577f4c8458
NyaunchoDavid <donchuchoh@gmail.com>
2019-02-18 16:59:50 +0300

    add html and js style

---
+++ pro.js
@@ -0,0 +1,54 @@
+function showOn1(){
+  var jan = document.getElementById('mon');
+  var ary = jan.src="dation.jpg";
+  var ary = jan.style.cursor = "pointer";
+}
+function showOff1(){
+  var jan = document.getElementById('mon');
+  var ary = jan.src = "date.jpg";
+}
+function showOn2(){
+  var feb = document.getElementById("tues");
+  var febr = feb.src = "sino.jpg";
+  var febr = feb.style.cursor = "pointer";
+}
+function showOff2(){
+  var feb = document.getElementById("tues");
+  var febr = feb.src = "cas.jpg";
+}
+function showOn3(){
+  var mar = document.getElementById("wed");
+  var march = mar.src = "pool.jpg";
+  var march = mar.style.cursor = "pointer";
+}
+function showOff3(){
+  var mar = document.getElementById("wed");
+  var march = mar.src = "swim.jpg";
+}
+function showOn4(){
+  var ap = document.getElementById("thur");
+  var april = ap.src = "hall.jpg";
+  var april = ap.style.cursor = "pointer";
+}
+function showOff4(){
+  var ap = document.getElementById("thur");
+  var april = ap.src = "conf.jpg";
+}
+function showOn5(){
+  var ju = document.getElementById("fri");
+  var june = ju.src = "grill.jpg";
+  var june = ju.style.cursor = "pointer";
+}
+function showOff5(){
+  var ju = document.getElementById("fri");
+  var june = ju.src = "bar.jpg";
+}
+function showOn6(){
+  var jul = document.getElementById("sat");
+  var july = jul.src = "break.jpeg";
+  var july = jul.style.cursor = "pointer";
+}
+function showOff6(){
+  var jul = document.getElementById("sat");
+  var july = jul.src = "fast.jpg";
+}
